63463a856d0b544843b70571eed0f55e0f18b355
Jangle Mangle <Jangle@mangle.com>
2025-03-24 19:03:44 +0530

    new UI

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-fetch('https://icanhazdadjoke.com/slack')
-.then(data=>data.json())
-.then(jokeData=>{
-    const jokeText=jokeData.attachments[0].text;
-    const jokeElement=document.getElementById('jokeElement');
+document.addEventListener("DOMContentLoaded", getJoke);
+document.getElementById("newJokeBtn").addEventListener("click", getJoke);
 
-    jokeElement.innerHTML=jokeText;
+async function getJoke() {
+  const jokeElement = document.getElementById("jokeElement");
+  jokeElement.innerHTML =
+    '<i class="fa-solid fa-spinner animate-spin"></i> Loading...';
 
-})
+  try {
+    let response = await fetch("https://icanhazdadjoke.com/", {
+      headers: { Accept: "application/json" },
+    });
+    let data = await response.json();
+
+    // Set joke inside the div (below button)
+    jokeElement.textContent = data.joke;
+  } catch (error) {
+    jokeElement.textContent = "Oops! Something went wrong 😢";
+  }
+}
